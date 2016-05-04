746a0b2969b86aca5b7374cf3b45475d9212cfab
crsilva5 <cl3iton357@gmail.com>
2016-05-04 20:29:11 -0300

    Metodo para incluir cadastro

--- src/main/ImplentDao.java
+++ src/main/ImplentDao.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
@@ -23,6 +24,27 @@ public class ImplentDao {
 		this.impl = impl;
 	}
 	public void salvar(Object t) {
+		PreparedStatement insert = getImp().getSqlInsert(getImp().getCon(), t);
+
+		System.out.println("INCLUINDO REGISTRO");
+
+		Cliente cliente = (Cliente) t;
+		try {
+			insert.setInt(1, cliente.getId());
+			insert.setString(2, cliente.getNome());
+			insert.setString(3, cliente.getEndereco());
+			insert.setString(4, cliente.getTelefone());
+			insert.setInt(5, cliente.getEstadoCivil().ordinal());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+
+		try {
+			insert.executeUpdate();
+			System.out.println("REGISTRO INCLUIDO!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
